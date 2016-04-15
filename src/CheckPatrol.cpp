3f7ba2b285c0742ae0db0658a9c90094b86bdb03
rzsavilla <rzsavilla@googlemail.com>
2016-04-16 00:46:36 +0100

    Fix for tank freeze after dodging

--- src/CheckPatrol.cpp
+++ src/CheckPatrol.cpp
@@ -28,22 +28,20 @@ Reposition_Action::Reposition_Action(TankControl * ptr_tank) {
 }
 
 bool Reposition_Action::run() {
-	if (!tank->bIsDodging) {		//Ensure tank is not attempting to dodge when reposition
-		if (tank->reachedDesiredPos() || tank->hasCollided()) {			// When tank should reposition
-			tank->bIsMoving = false;
-			if (tank->hasCollided()) { tank->goBackward(); }			// Move away from collision slightly
-			std::cout << "Reposition\n";
-		}
-
-		if (!tank->bIsMoving) {						//Reposition tank
-			fX = (float)(rand() % 750 + 10);		//Generate random position
-			fY = (float)(rand() % 540 + 10);
-			//std::cout << "Repositioning\n";
-			tank->bIsMoving = true;					//Tank has is now moving
-		}
-		tank->patrolTurret();
-		tank->setDesiredPosition(fX, fY);
+	if (tank->reachedDesiredPos() || tank->hasCollided()) {			// When tank should reposition
+		tank->bIsMoving = false;
+		if (tank->hasCollided()) { tank->goBackward(); }			// Move away from collision slightly
+		std::cout << "Reposition\n";
 	}
+
+	if (!tank->bIsMoving) {						//Reposition tank
+		fX = (float)(rand() % 750 + 10);		//Generate random position
+		fY = (float)(rand() % 540 + 10);
+		//std::cout << "Repositioning\n";
+		tank->bIsMoving = true;					//Tank has is now moving
+	}
+	tank->patrolTurret();
+	tank->setDesiredPosition(fX, fY);
 	return true;
 }
 
