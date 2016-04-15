e9faf70e611a84c288ee8783e73cedb391ea250a
rzsavilla <rzsavilla@googlemail.com>
2016-04-15 21:43:34 +0100

    Fix base position stored no longer a pair, iterator fix for check shell collision with friendly building

--- src/TankControl.cpp
+++ src/TankControl.cpp
@@ -154,12 +154,12 @@ void TankControl::markTarget(Position p) {
 	//Enemy base spotted
 	enemyBasePos = p;
 	if (vEnemyBasePos.size() <= 0) {
-		vEnemyBasePos.push_back(std::pair<bool,Position>(true,p));
+		vEnemyBasePos.push_back(p);
 		std::cout << "New  Enemy Building:" << " x:" << (int)p.getX() << " y:" << (int)p.getY() << std::endl;
 	}
 	else {
 		if (!findMatch(p, vEnemyBasePos)) {
-			vEnemyBasePos.push_back(std::pair<bool, Position>(true, p));
+			vEnemyBasePos.push_back(p);
 			std::cout << "New  Enemy Building:" << " x:" << (int)p.getX() << " y:" << (int)p.getY() << std::endl;
 		}
 	}
@@ -187,12 +187,12 @@ void TankControl::markBase(Position p) {
 	//Base spotted
 	//Loops through bases found and compares the base spotted
 	if (vBasePos.size() <= 0) {
-		vBasePos.push_back(std::pair<bool, Position>(true, p));
+		vBasePos.push_back(p);
 		//std::cout << "New Building:" << " x:" << (int)p.getX() << " y:" << (int)p.getY() << std::endl;
 	}
 	else {
 		if (!findMatch(p, vBasePos)) {		//Iterate through bases spotted and check if they have already been found
-			vBasePos.push_back(std::pair<bool, Position>(true, p));
+			vBasePos.push_back(p);
 			//std::cout << "New Building:" << " x:" << (int)p.getX() << " y:" << (int)p.getY() << std::endl;
 		}
 	}
@@ -365,15 +365,13 @@ bool TankControl::willShellHit(Position pshell, Position pprevShell)
 
 bool TankControl::willShellHitFreindlyBuilding()
 {
-	for (it = vBasePos.begin(); it < vBasePos.end(); it++)
+	for (auto it = vBasePos.begin(); it < vBasePos.end(); it++)
 	{
-	
-
 		if (bEnemySpotted && bBaseSpotted)// both are in vision
 		{
 			myVector freindlyTank(pos.getX(), pos.getY());
 			float aimingAt = turretTh;
-			myVector Building(vBasePos[i].second.getX(), vBasePos[i].second.getY());
+			myVector Building(it->getX(), it->getY());
 			myVector enemeyTank(enemyCurrPos.getX(), enemyCurrPos.getY());
 			myVector dist;
 			float distanceTotank;
@@ -390,10 +388,10 @@ bool TankControl::willShellHitFreindlyBuilding()
 			bool bShellIsGoingVertical = false;
 
 			//find corrds of buidling bounding box
-			myVector topLeft(vBasePos[i].second.getX(), vBasePos[i].second.getY());
-			myVector topRight((vBasePos[i].second.getX())+20, vBasePos[i].second.getY());
-			myVector bottomLeft(vBasePos[i].second.getX(), (vBasePos[i].second.getY()) + 20);
-			myVector bottomRight((vBasePos[i].second.getX()) + 20, (vBasePos[i].second.getY()) + 20);
+			myVector topLeft(it->getX(), it->getY());
+			myVector topRight((it->getX())+20, it->getY());
+			myVector bottomLeft(it->getX(), (it->getY()) + 20);
+			myVector bottomRight((it->getX()) + 20, (it->getY()) + 20);
 
 			float x;
 			float y;
