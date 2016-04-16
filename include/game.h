2a578a7355383ad51f95872c177cfe8128029ed9
rzsavilla <rzsavilla@googlemail.com>
2016-04-16 20:37:00 +0100

    Revert AI player back to player

--- include/game.h
+++ include/game.h
@@ -15,32 +15,32 @@ using namespace std;
 
 class Game : public sf::Drawable
 {
-	private:
-		unsigned short lives; // number of lives left
-		bool debugMode; // toggle for debug mode
-		sf::RectangleShape background; // Background the playing area
-		sf::RectangleShape ammoArea; // Background where the ammo is drawn
-		list<Obstacle> obstacles; // Obstacles in the tanks way
-		list<Obstacle> blueBuildings; // Collection of blue buildings
-		list<Obstacle> redBuildings; // Collection of red buildings
-		list<Shell> shells; // Shells fired from tanks
-		void resetNpc(); // Move the NPC after it has been shot
-		void resetPlayer(); // Move the player after it has been shot
-		void fireShell(Position fp, bool npc); // Fire a shell
-		int redScore; // Score of red tank
-		int blueScore; // Score of blue tank
-    public:
-		Game(); // Constructor
-		~Game(); // Destructor
-		virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const; // Draw the game
-		void play(); // Play the game for one timestep
-		//DumbTank npc; // Red tank
-		TankMind npc;	// AI Red tank
-		TankMind player; // Blue tank
-		void keyPressed(sf::Keyboard::Key key); // function for processing input
-		void keyReleased(sf::Keyboard::Key key); // function for processing input
-		bool gameOver() const; // Has the game finished?
-		int numBlueBuildings() const; // Count of blue buildings
-		int numRedBuildings() const; // Count of red buildings
+private:
+	unsigned short lives; // number of lives left
+	bool debugMode; // toggle for debug mode
+	sf::RectangleShape background; // Background the playing area
+	sf::RectangleShape ammoArea; // Background where the ammo is drawn
+	list<Obstacle> obstacles; // Obstacles in the tanks way
+	list<Obstacle> blueBuildings; // Collection of blue buildings
+	list<Obstacle> redBuildings; // Collection of red buildings
+	list<Shell> shells; // Shells fired from tanks
+	void resetNpc(); // Move the NPC after it has been shot
+	void resetPlayer(); // Move the player after it has been shot
+	void fireShell(Position fp, bool npc); // Fire a shell
+	int redScore; // Score of red tank
+	int blueScore; // Score of blue tank
+public:
+	Game(); // Constructor
+	~Game(); // Destructor
+	virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const; // Draw the game
+	void play(); // Play the game for one timestep
+	//DumbTank npc; // Red tank
+	TankMind npc;	// AI Red tank
+	PlayerTank player; // Blue tank
+	void keyPressed(sf::Keyboard::Key key); // function for processing input
+	void keyReleased(sf::Keyboard::Key key); // function for processing input
+	bool gameOver() const; // Has the game finished?
+	int numBlueBuildings() const; // Count of blue buildings
+	int numRedBuildings() const; // Count of red buildings
 };
 #endif
