5918a89441ee3e4bc1ba0033c6e0781dca99265d
rzsavilla <rzsavilla@googlemail.com>
2016-04-18 19:41:46 +0100

    Added unit tests

---
+++ UnitTest/stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
